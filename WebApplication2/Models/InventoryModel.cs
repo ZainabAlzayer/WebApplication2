9ea08d5d9861c965f7c04bf0ed4ebb55aab8baec
ZainabAlzayer <zalzaye3@calstatela.edu>
2015-11-07 21:56:29 -0800

    HW5

---
+++ WebApplication2/Models/InventoryModel.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+using System.Web.Mvc;
+using System.Collections;
+
+namespace WebApplication2.Models
+{
+    public class InventoryModel
+    {
+       
+
+        public int ID { get; set; } 
+         [Required(ErrorMessage = "Please enter a code!")] 
+         public string Code { get; set; } 
+        
+         public string Description { get; set; } 
+         [Required(ErrorMessage = "Please select a valid category")] 
+
+        public string Category { get; set; }
+
+        [Required(ErrorMessage = "Please Enter Name!")]
+        public string Name { get; set; }
+
+
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
